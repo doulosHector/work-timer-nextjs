3e8c037d9583aaa3a24326eb5bcf148eaec5ab25
Héctor Ortiz <hector.ortiz7@outlook.com>
2024-07-10 18:35:22 -0600

    feat: add timer digits component

---
+++ src/app/components/atoms/TimeDigits.tsx
@@ -0,0 +1,14 @@
+import { formatTime } from "@/app/utils/formatTime";
+
+const TimeDigits = ({ label, time }: { label: string; time: number }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <span className="text-6xl font-bold text-gray-800">
+        {formatTime(time)}
+      </span>
+      <span className="text-sm text-gray-500">{label}</span>
+    </div>
+  );
+};
+
+export default TimeDigits;
