6a4abd5d402a74ab159a6f79b715d1943ec3ffa1
Héctor Ortiz <hector.ortiz7@outlook.com>
2024-07-19 18:09:30 -0600

    ref: change color of progress bar based on progress

--- src/app/components/atoms/ProgressBar.tsx
+++ src/app/components/atoms/ProgressBar.tsx
@@ -1,22 +1,16 @@
 const ProgressBar = ({
   progress,
-  color,
   showPercentage = false,
 }: {
   progress: number;
-  color: "blue" | "red" | "yellow" | "green";
   showPercentage?: boolean;
 }) => {
-  const colorStyles = {
-    blue: "bg-blue-600",
-    red: "bg-red-600",
-    yellow: "bg-yellow-600",
-    green: "bg-green-600",
-  };
   return (
     <div className="w-full bg-gray-200 rounded-full h-3 dark:bg-gray-700 mt-4">
       <div
-        className={`h-3 rounded-full ${colorStyles[color]} transition-all duration-500`}
+        className={`h-3 rounded-full transition-all duration-500 ${
+          progress < 100 ? "bg-blue-600" : "bg-green-600"
+        }`}
         style={{ width: `${progress}%` }}
       ></div>
       {showPercentage && (
