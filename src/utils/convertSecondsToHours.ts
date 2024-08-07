baf7b7e43f97fd89fb9f086233408c77a77c437f
Héctor Ortiz <hector.ortiz7@outlook.com>
2024-08-06 19:10:34 -0600

    fix: show zero when no time converting seconds to hours

--- src/utils/convertSecondsToHours.ts
+++ src/utils/convertSecondsToHours.ts
@@ -1,7 +1,7 @@
 // Util to convert seconds to hours and minutes
 export const convertSecondsToHours = (secondsParam: number) => {
-  const minutes = Math.floor((secondsParam % 3600) / 60);
-  const hours = Math.floor(secondsParam / 3600);
-  const seconds = secondsParam % 60;
+  const minutes = Math.floor((secondsParam % 3600) / 60) || 0;
+  const hours = Math.floor(secondsParam / 3600) || 0;
+  const seconds = secondsParam % 60 || 0;
   return { hours, minutes, seconds };
 };
